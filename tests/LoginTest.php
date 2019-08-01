5647f0522e0636b2e7fd1e9b6c0e43623a313513
eLdj <eldji22@hotmail.fr>
2019-08-01 11:56:52 +0000

    test installés

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -6,8 +6,40 @@ use PHPUnit\Framework\TestCase;
 
 class LoginTest extends TestCase
 {
-    public function testSomething()
+    // public function testSomething()
+    // {
+    //     $this->assertTrue(true);
+    // }
+
+    // public function testIndex()
+    // {
+    //     $client = static::createClient();
+    //     $crawler = $client->request('GET', '/api/employes');
+    //     $rep=$client->getResponse();
+    //     $this->assertSame(200,$client->getResponse()->getStatusCode());
+    //     //$this->assertJsonStringEqualsJsonString($jsonstring,$rep->getContent());
+    // }
+    public function register()
     {
-        $this->assertTrue(true);
+        $client = static::createClient([],[
+            'PHP_AUTH_USER'=> 'admin' ,
+             'PHP_AUTH_PW'=>'admin'
+          ]);
+        $crawler = $client->request('POST', '/api/register',[],[],
+        ['CONTENT_TYPE'=>"application/json"],
+        '{"username":"admin3", "password":"admin"}');
+        $rep=$client->getResponse();
+        var_dump($rep);
+        $this->assertSame(201,$client->getResponse()->getStatusCode());
     }
+    // public function testAjoutKo()
+    // {
+    //     $client = static::createClient();
+    //     $crawler = $client->request('POST', '/api/employe',[],[],
+    //     ['CONTENT_TYPE'=>"application/json"],
+    //     '{"matricule":"004","nom": "ndiaye","prenom": "","salaire": "iiii"}');
+    //     $rep=$client->getResponse();
+    //     var_dump($rep);
+    //     $this->assertSame(200,$client->getResponse()->getStatusCode());
+    // }
 }
